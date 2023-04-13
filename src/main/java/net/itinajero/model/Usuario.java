6de2a45dde26264c6d392c9ec978fee4c512e0af
utn211241050 <101812441+utn211241050@users.noreply.github.com>
2023-04-13 13:11:29 -0600

    Update Usuario.java

--- src/main/java/net/itinajero/model/Usuario.java
+++ src/main/java/net/itinajero/model/Usuario.java
@@ -34,7 +34,7 @@ public class Usuario {
 	// Relacion ManyToMany (Un usuario tiene muchos perfiles)
 	// Por defecto Fetch es FetchType.LAZY
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "UsuarioPerfil", // tabla intermedia
+	@JoinTable(name = "usuarioperfil", // tabla intermedia
 			joinColumns = @JoinColumn(name = "idUsuario"), // foreignKey en la tabla de UsuarioPerfil
 			inverseJoinColumns = @JoinColumn(name = "idPerfil") // foreignKey en la tabla de UsuarioPerfil
 	)
