182396b5e557a441cbb5f8c01ac8afd409431d05
utn211241050 <101812441+utn211241050@users.noreply.github.com>
2023-04-13 21:42:44 -0600

    Update WebConfig.java

--- src/main/java/net/itinajero/config/WebConfig.java
+++ src/main/java/net/itinajero/config/WebConfig.java
@@ -20,7 +20,8 @@ public class WebConfig implements WebMvcConfigurer {
 		// Configuración de los recursos estáticos (imagenes de las vacantes) 
 		//registry.addResourceHandler("/logos/**").addResourceLocations("file:c:/empleos/img-vacantes/"); // Windows
 		//registry.addResourceHandler("/logos/**").addResourceLocations("file:/empleos/img-vacantes/"); // Linux
-		registry.addResourceHandler("/logos/**").addResourceLocations("file:"+rutaImagenes); 
+		//registry.addResourceHandler("/logos/**").addResourceLocations("file:"+rutaImagenes); 
+		registry.addResourceHandler("/logos/**").addResourceLocations("file:c:/empleos/img-vacantes/");
 		
 		// Configuración de los recursos estáticos (archivos de los CV)
 		//registry.addResourceHandler("/cv/**").addResourceLocations("file:c:/empleos/files-cv/"); // Windows
